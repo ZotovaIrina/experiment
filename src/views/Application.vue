586621b3a68a8d408cda6975c26cca64e3b479eb
irinazotova <zotovaiv87@gmail.com>
2018-07-22 20:05:47 -0400

    Use vuex-class for component

--- src/views/Application.vue
+++ src/views/Application.vue
@@ -5,8 +5,8 @@
         <!--</h2>-->
         <!--<calendar :input-date="currentDate"-->
         <!--:set-new-date="setNewDate"/>-->
-        <SelectOption :options="getDeedDocuments"
-                      :input-value="currentDeedDocumentType"
+        <SelectOption :options="getDeedDocumentTypes"
+                      :input-value="getDeedDocumentType"
                       @changeValue="setNewDeedDocumentType"/>
     </div>
 </template>
@@ -14,63 +14,21 @@
 <script lang="ts">
 
     import {Component, Prop, Vue} from 'vue-property-decorator';
+    import {Getter, Action} from 'vuex-class';
     // import Calendar from '../components/fields/Calendar';
-    import HelloWorld from '@/components/HelloWorld.vue';
     import SelectOption from '@/components/fields/SelectOption.vue';
-    import {DeedDocumentEnum} from '../store/enams/DeedDocumentType';
 
     @Component({
         components: {SelectOption}
     })
     export default class Application extends Vue {
+        @Getter public getDeedDocumentTypes: string[];
+        @Getter public getDeedDocumentType: string;
+        @Action public setNewDocumentType: (newValue: string) => void;
 
         public setNewDeedDocumentType(newValue: string) {
-            this.$store.dispatch('setNewDocumentType', newValue);
+            this.setNewDocumentType(newValue);
         }
-
-        get getDeedDocuments(): string[] {
-            return this.$store.getters.getDeedDocumentTypes;
-        }
-
-        get currentDeedDocumentType(): string {
-            return this.$store.getters.getDeedDocumentType;
-        }
-
 
     }
 </script>
-
-<!--import Calendar from '../components/fields/Calendar';-->
-<!--import SelectOption from '../components/fields/SelectOption';-->
-<!--import deedDocumentTypes, {default as DeedDocumentType} from '../store/enams/DeedDocumentType';-->
-
-<!--export default {-->
-<!--name: 'HelloWorld',-->
-<!--components: {-->
-<!--Calendar,-->
-<!--SelectOption-->
-<!--},-->
-<!--data() {-->
-<!--return {-->
-<!--msg: 'Welcome to Your Vue.js App',-->
-<!--options: {weekday: 'long', year: 'numeric', month: 'long', day: '2-digit'},-->
-<!--deedDocumentTypes-->
-<!--};-->
-<!--},-->
-<!--computed: {-->
-<!--currentDate() {-->
-<!--return this.$store.state.currentDate;-->
-<!--},-->
-<!--currentDeedDocumentType() {-->
-<!--return this.$store.state.deedDocumentType;-->
-<!--}-->
-<!--},-->
-<!--methods: {-->
-<!--setNewDate(newDate) {-->
-<!--this.$store.commit('setNewDate', newDate);-->
-<!--},-->
-<!--setNewDeedDocumentType(newValue) {-->
-<!--this.$store.commit('setNewDocumentType', newValue);-->
-<!--}-->
-<!--}-->
-<!--};-->
